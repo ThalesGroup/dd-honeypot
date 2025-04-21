3f1cbfe244eada1e6f52d45fb339560d20bb1248
MohamedFaheemM <mdfaheempasha@gmail.com>
2025-04-21 12:13:54 +0530

    modified ssh honeypot test

--- src/ssh_honeypot.py
+++ src/ssh_honeypot.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        logging.FileHandler('../logs/honeypot.log'),
+        logging.FileHandler('/Users/mohamed.faheem/dd-honeypot/logs/honeypot.log'),
         logging.StreamHandler()
     ]
 )
