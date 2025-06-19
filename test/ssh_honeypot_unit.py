a143241f03330e88921124840183a91889c32d57
MohamedFaheemM <mdfaheempasha@gmail.com>
2025-06-19 13:30:37 +0530

    fake fs file missing error fixed

--- test/ssh_honeypot_unit.py
+++ test/ssh_honeypot_unit.py
@@ -232,16 +232,3 @@ def test_scp_upload(ssh_honeypot_with_fakefs, tmp_path):
         scp.put(str(test_file), "/test_uploaded.txt")
 
     client.close()
-
-
-def test_fakefs_session_unwrapped(ssh_honeypot_with_fakefs, tmp_path):
-    handler = FakeFSDataHandler(
-        data_file="testdata/data.jsonl",
-        system_prompt="dummy",
-        model_id="gpt-3.5",
-        fs_file="honeypots/busybox/fs_busybox.json",
-    )
-    chained = ChainedDataHandler([handler])
-    session = chained.connect({"username": "u", "password": "p"})
-
-    assert isinstance(session["fs"], FakeFileSystem), "session['fs'] was not unwrapped"
