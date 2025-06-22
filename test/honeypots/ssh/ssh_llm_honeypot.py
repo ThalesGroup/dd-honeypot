dd7a1cdeb08c613b9004c0d8acdfb7403a0bd394
MohamedFaheemM <mdfaheempasha@gmail.com>
2025-06-22 23:01:57 +0530

    ssh dir removed from honeypots

--- test/honeypots/ssh/ssh_llm_honeypot.py
+++
@@ -1,7 +0,0 @@
-import os
-from ssh_llm_honeypot_core import DataSSHHoneypot  # You will define this core class elsewhere
-
-class SSHLLMHoneypot(DataSSHHoneypot):
-    def __init__(self, port: int = 2222):
-        folder = os.path.dirname(__file__)
-        super().__init__(folder, port)
