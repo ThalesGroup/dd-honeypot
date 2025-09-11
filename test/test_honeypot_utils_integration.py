448e10bff03478f4f278626b1c804f9e10379250
MohamedFaheemM <mdfaheempasha@gmail.com>
2025-09-11 15:37:50 +0530

    Updated ids in int. test and added http disp. test

--- test/test_honeypot_utils_integration.py
+++ test/test_honeypot_utils_integration.py
@@ -12,8 +12,8 @@ def set_aws_api_key():
 @pytest.mark.parametrize(
     "model_id",
     [
-        "anthropic.claude-instant-v1",
-        "anthropic.claude-v2",
+        "anthropic.claude-3-haiku-20240307-v1:0",
+        "anthropic.claude-3-sonnet-20240229-v1:0",
         "anthropic.claude-3-5-sonnet-20240620-v1:0",
     ],
 )
