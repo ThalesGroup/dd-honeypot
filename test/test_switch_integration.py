903a511b94f0e82387b03fe0b1a873cc2f5a2d1e
MohamedFaheemM <mdfaheempasha@gmail.com>
2025-12-02 11:38:49 +0530

    Try to fix timeout failure by increasing timeout

--- test/test_switch_integration.py
+++ test/test_switch_integration.py
@@ -28,7 +28,7 @@ def connect_and_run_ssh_commands(
     client.set_missing_host_key_policy(AutoAddPolicy())
     client.connect("127.0.0.1", port=port, username=username, password=password)
     shell = client.invoke_shell()
-    shell.settimeout(5.0)
+    shell.settimeout(60.0)
 
     # Read initial prompt
     buffer = ""
@@ -47,7 +47,7 @@ def connect_and_run_ssh_commands(
             buffer += shell.recv(1024).decode()
             if re.search(prompt_regex, buffer):
                 break
-            if time.time() - start > 5:
+            if time.time() - start > 60:
                 raise TimeoutError(f"Timeout waiting for prompt after {cmd}")
         # Strip off the final prompt from buffer
         output = re.sub(rf".*{re.escape(cmd)}\r\n", "", buffer)
